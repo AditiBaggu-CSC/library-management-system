b636b82f25a6deb396ca034b13a6078861e304f5
Aditi Baggu <aditibaggu@gmail.com>
2024-07-02 18:54:42 +0530

    commented account token

--- twilioConfig.js
+++ twilioConfig.js
@@ -1,7 +1,7 @@
 const twilio = require("twilio");
 
-const accountSid = "ACb9f5b73062e62c6ca54631af6837aff1";
-const authToken = "676c7b2d110f1fec17e535c326520979";
+//const accountSid = "ACb9f5b73062e62c6ca54631af6837aff1";
+//const authToken = "676c7b2d110f1fec17e535c326520979";
 const client = new twilio(accountSid, authToken);
 
 // Ensure this is a verified Twilio WhatsApp number
